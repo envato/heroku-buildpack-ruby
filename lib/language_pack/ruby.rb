3375744bf086f3aa6bfd875280bc3af2e79d053f
Jack Chen (chendo) <me@chen.do>
2012-02-28 14:14:10 +1100

    Trying something else

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -297,7 +297,7 @@ ERROR
         pwd            = run("pwd").chomp
         # we need to set BUNDLE_CONFIG and BUNDLE_GEMFILE for
         # codon since it uses bundler.
-        env_vars       = "env BUNDLE_GEMFILE=#{pwd}/Gemfile BUNDLE_CONFIG=#{pwd}/.bundle/config CPATH=#{yaml_include}:#{exiv2_include}:$CPATH CPPPATH=#{yaml_include}:#{exiv2_include}:$CPPPATH LIBRARY_PATH=#{exiv2_lib}:#{yaml_lib}:$LIBRARY_PATH RUBYOPT=\"#{syck_hack}\""
+        env_vars       = "env BUNDLE_GEMFILE=#{pwd}/Gemfile BUNDLE_CONFIG=#{pwd}/.bundle/config CPATH=#{yaml_include}:#{exiv2_include}:$CPATH CPPPATH=#{yaml_include}:#{exiv2_include}:$CPPPATH CPPFLAGS=-I#{exiv2_include} LIBRARY_PATH=#{exiv2_lib}:#{yaml_lib}:$LIBRARY_PATH LD_LIBRARY_PATH=$LIBRARY_PATH RUBYOPT=\"#{syck_hack}\""
         puts "Env: #{env_vars}"
         puts "Running: #{bundle_command}"
         bundler_output << pipe("#{env_vars} #{bundle_command} --no-clean 2>&1")
